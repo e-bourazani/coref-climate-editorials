e3e99251564235fbc54a868a7988e4f444f6cf3b
e-bourazani <evelinazani48@gmail.com>
2026-02-16 15:22:48 +0100

    stanza salient entities

--- coref_utils.py
+++ coref_utils.py
@@ -12,25 +12,21 @@ def compute_salient_entities(corpus, min_freq=2):
 
     for article in corpus:
         art_id = article["article_id"]
-
         for prgr in article["paragraphs"]:
             for ann in prgr["annotations"]:
                 entity = ann["text"].lower().strip()
                 role = ann["role"]
                 key = (entity, role)
-
                 role_counts[art_id][key] += 1
 
     salient_entities = {}
 
     for art_id, counts in role_counts.items():
         salient_entities[art_id] = set()
-
         for (entity, role), n in counts.items():
             if n >= min_freq:
                 salient_entities[art_id].add(entity)
                 entity_role[art_id][entity] = role
-
     return salient_entities, entity_role
 
 
@@ -40,19 +36,15 @@ def filter_salient_clusters(coref_data, salient_entities):
     """
 
     filtered_output = []
-
     for article in coref_data:
         art_id = article["article_id"]
         clusters = article["clusters"]
-
         filtered = [
             cl for cl in clusters
             if cl["entity"] in salient_entities.get(art_id, set())
         ]
-
         filtered_output.append({
             "article_id": art_id,
             "clusters": filtered
         })
-
     return filtered_output
---
+++ make_salient_entities.py
@@ -0,0 +1,25 @@
+import json
+from coref_utils import compute_salient_entities, filter_salient_clusters
+
+
+with open("data/NatSci_annotated.json") as f:
+    corpus = json.load(f)
+salient_entities, _ = compute_salient_entities(corpus)
+
+#fcoref
+with open("model_outputs/fcoref_annotated_only.json") as f:
+    fcoref_data = json.load(f)
+fcoref_salient = filter_salient_clusters(fcoref_data, salient_entities)
+
+with open("model_outputs/fcoref_salient_only.json", "w") as f:
+    json.dump(fcoref_salient, f, indent=2)
+
+
+#stanza
+with open("model_outputs/stanza_annotated_only.json") as f:
+    stanza_data = json.load(f)
+stanza_salient = filter_salient_clusters(stanza_data, salient_entities)
+
+with open("model_outputs/stanza_salient_only.json", "w") as f:
+    json.dump(stanza_salient, f, indent=2)
+print("Stanza salient file created.")
